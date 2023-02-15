f97b213f10cfac5aa201fcea77f4af1742afe11e
Andrew Bates <andrew.bates@sap.com>
2023-02-15 15:44:23 -0500

    Added python package installer to example dev container

---
+++ examples/example_design/.devcontainer/install_additional.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+
+WHEEL_DIR="/tmp/wheels"
+
+if [ -z "$(ls $WHEEL_DIR)" ]; then
+    echo "Installing packages from network"
+    pip3 install -r /tmp/requirements.txt
+else
+    echo "Installing wheels from local files"
+    pip3 install --no-index --find-links="${WHEEL_DIR}/" -r /tmp/requirements.txt
+fi
