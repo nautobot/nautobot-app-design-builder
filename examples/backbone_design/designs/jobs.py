dc5bb914fa1a922f7aa929bd674f903fba64feef
Andrew Bates <andrew.bates@networktocode.com>
2024-01-03 13:10:34 -0500

    docs: Additional example for blog post.

--- examples/backbone_design/designs/jobs.py
+++ examples/backbone_design/designs/jobs.py
@@ -2,8 +2,10 @@
 
 from .initial_data.jobs import InitialDesign
 from .core_site.jobs import CoreSiteDesign
+from .edge_site.jobs import EdgeDesign
 
 __all__ = (
     "InitialDesign",
     "CoreSiteDesign",
+    "EdgeDesign",
 )
