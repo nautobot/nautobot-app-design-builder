432460e2cdec1c22c726dccba66319f5c300a9b9
Andrew Bates <andrew.bates@networktocode.com>
2024-05-13 08:50:20 -0400

    refactor: Refactored builder_test_case decorator
    The builder test case is now a base class instead of a decorator. This
    cleans up the layout of the code a bit making it easier to follow the
    logic.

--- nautobot_design_builder/contrib/tests/test_ext.py
+++ nautobot_design_builder/contrib/tests/test_ext.py
@@ -2,11 +2,10 @@
 
 import os
 
-from django.test import TestCase
-
-from nautobot_design_builder.tests.test_builder import builder_test_case
+from nautobot_design_builder.tests.test_builder import BuilderTestCase
 
 
-@builder_test_case(os.path.join(os.path.dirname(__file__), "testdata"))
-class TestAgnosticExtensions(TestCase):
+class TestAgnosticExtensions(BuilderTestCase):
     """Test contrib extensions against any version of Nautobot."""
+
+    data_dir = os.path.join(os.path.dirname(__file__), "testdata")
