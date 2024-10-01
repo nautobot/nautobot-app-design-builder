79668f61a68b807d8957bfdccd5d17396133991b
Andrew Bates <andrew.bates@networktocode.com>
2024-10-01 09:04:00 -0400

    fix: Fixes most likely issues with attribute collisions
    This fixes an issue where some Nautobot models have an `environment` or `metadata` field name. Almost all instance attributes in `ModelInstance` have been moved into the `ModelMetadata` class, and other fields have been prefixed to reduce the likelyhood of collision.
    
    Fixes #160

--- nautobot_design_builder/contrib/ext.py
+++ nautobot_design_builder/contrib/ext.py
@@ -66,7 +66,7 @@ class LookupMixin:
                 yield from LookupMixin._flatten(value, f"{prefix}{key}__")
             else:
                 if isinstance(value, ModelInstance):
-                    yield (f"!get:{prefix}{key}", value.instance)
+                    yield (f"!get:{prefix}{key}", value.design_instance)
                 else:
                     yield (f"!get:{prefix}{key}", value)
 
@@ -121,7 +121,7 @@ class LookupMixin:
         try:
             model_class = self.environment.model_class_index[queryset.model]
             if parent:
-                return parent.create_child(model_class, query, queryset)
+                return parent.design_metadata.create_child(model_class, query, queryset)
             return model_class(self.environment, query, queryset)
         except ObjectDoesNotExist:
             # pylint: disable=raise-missing-from
@@ -289,9 +289,9 @@ class CableConnectionExtension(AttributeExtension, LookupMixin):
             {
                 "termination_a": model_instance,
                 "!create_or_update:termination_b_type_id": ContentType.objects.get_for_model(
-                    remote_instance.instance
+                    remote_instance.design_instance
                 ).id,
-                "!create_or_update:termination_b_id": remote_instance.instance.id,
+                "!create_or_update:termination_b_id": remote_instance.design_instance.id,
                 "deferred": True,
             }
         )
@@ -437,7 +437,7 @@ class ChildPrefixExtension(AttributeExtension):
         if parent is None:
             raise DesignImplementationError("the child_prefix tag requires a parent")
         if isinstance(parent, ModelInstance):
-            parent = str(parent.instance.prefix)
+            parent = str(parent.design_instance.prefix)
         elif not isinstance(parent, str):
             raise DesignImplementationError("parent prefix must be either a previously created object or a string.")
 
@@ -528,8 +528,8 @@ class BGPPeeringExtension(AttributeExtension):
         peering_a = None
         peering_z = None
         try:
-            peering_a = endpoint_a.instance.peering
-            peering_z = endpoint_z.instance.peering
+            peering_a = endpoint_a.design_instance.peering
+            peering_z = endpoint_z.design_instance.peering
         except self.Peering.model_class.DoesNotExist:
             pass
 
@@ -544,13 +544,13 @@ class BGPPeeringExtension(AttributeExtension):
                 peering_z.delete()
 
         retval["endpoints"] = [endpoint_a, endpoint_z]
-        endpoint_a.metadata.attributes["peering"] = model_instance
-        endpoint_z.metadata.attributes["peering"] = model_instance
+        endpoint_a.design_metadata.attributes["peering"] = model_instance
+        endpoint_z.design_metadata.attributes["peering"] = model_instance
 
         def post_save():
             peering_instance: ModelInstance = model_instance
-            endpoint_a = peering_instance.instance.endpoint_a
-            endpoint_z = peering_instance.instance.endpoint_z
+            endpoint_a = peering_instance.design_instance.endpoint_a
+            endpoint_z = peering_instance.design_instance.endpoint_z
             endpoint_a.peer, endpoint_z.peer = endpoint_z, endpoint_a
             endpoint_a.save()
             endpoint_z.save()
