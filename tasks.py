c717ba935f13995f64dd9e9772415908362c321a
Stephen Kiely <stephenkiely87@gmail.com>
2025-12-02 09:12:10 -0600

    Pin CI nautobot versions

--- tasks.py
+++ tasks.py
@@ -52,7 +52,7 @@ namespace = Collection("nautobot_design_builder")
 namespace.configure(
     {
         "nautobot_design_builder": {
-            "nautobot_ver": "2.4.20",
+            "nautobot_ver": "2.4.23",
             "project_name": "nautobot-design-builder",
             "python_ver": "3.12",
             "local": False,
