7214f4457a575fc987903bede5aef948d2249a82
Nikos Kallergis <nikolaos.kallergis@gmail.com>
2026-01-23 19:33:56 +0200

    Add panels to various models for Design Deployment membership

--- nautobot_design_builder/template_content.py
+++ nautobot_design_builder/template_content.py
@@ -2,8 +2,58 @@
 
 from django.conf import settings
 from django.urls import reverse
-from nautobot.extras.plugins import TemplateExtension
+from nautobot.apps.ui import ObjectsTablePanel, SectionChoices, TemplateExtension
+from nautobot.core.views.utils import get_obj_from_context
 from nautobot.extras.utils import registry
+
+from nautobot_design_builder.models import Deployment
+from nautobot_design_builder.tables import DeploymentTable
+
+
+class DeploymentObjectsTablePanel(ObjectsTablePanel):
+    """DataTablePanel for displaying Deployment data."""
+
+    def should_render(self, context):
+        """Determine if the panel should be rendered based on the presence of data in context."""
+        obj = get_obj_from_context(context)
+
+        # Calculate parent deployments for the object
+        parent_deployments = []
+        for deployment in Deployment.objects.all():
+            if obj in deployment.get_design_objects(type(obj)):
+                parent_deployments.append(deployment)
+
+        parent_deployments_table = DeploymentTable(parent_deployments)
+
+        # Hide columns not in include_columns
+        for column in parent_deployments_table.columns.all():
+            if column.name not in self.include_columns:
+                parent_deployments_table.columns.hide(column.name)
+        context.update({"parent_deployments": parent_deployments_table})
+
+        return bool(parent_deployments)
+
+
+def table_factory(content_type_label):
+    """Generate a ObjectsTablePanel object for a given content type."""
+
+    class DesignDeploymentMembers(TemplateExtension):  # pylint: disable=W0223
+        """Dynamically generated DesignDeploymentMembers class."""
+
+        model = content_type_label
+
+        object_detail_panels = [
+            DeploymentObjectsTablePanel(
+                weight=100,
+                section=SectionChoices.LEFT_HALF,
+                context_table_key="parent_deployments",
+                include_columns=["name", "design", "version", "status"],
+                max_display_count=10,
+                paginate=True,
+            ),
+        ]
+
+    return DesignDeploymentMembers
 
 
 def tab_factory(content_type_label):
@@ -29,15 +79,15 @@ def tab_factory(content_type_label):
 
 
 class DesignBuilderTemplateIterator:  # pylint: disable=too-few-public-methods
-    """Iterator that generates CustomValidator classes for each model registered in the extras feature query registry 'custom_validators'."""
+    """Iterator that generates ObjectsTablePanel classes for all objects beloging to a Design Deployment."""
 
     def __iter__(self):
-        """Return a generator of CustomValidator classes for each registered model."""
+        """Return a generator of ObjectsTablePanel classes for each registered model."""
         for app_label, models in registry["model_features"]["custom_validators"].items():
             for model in models:
+                yield table_factory(f"{app_label}.{model}")
                 if (app_label, model) in settings.PLUGINS_CONFIG["nautobot_design_builder"]["protected_models"]:
-                    label = f"{app_label}.{model}"
-                    yield tab_factory(label)
+                    yield tab_factory(f"{app_label}.{model}")
 
 
 template_extensions = DesignBuilderTemplateIterator()
